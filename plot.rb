d7f2d0a0677a1b476c80e89730dc54d3b7685fb1
read <read.sprabery@gmail.com>
2014-05-08 13:07:34 -0500

    Fixed plot.rb

--- plot.rb
+++ plot.rb
@@ -2,21 +2,26 @@
 
 require 'csv'
 
-file_names = Dir.entries('./results').select { |file| file.include? '.csv' }
-file_names.each do |file_name|
-  columns = 0
-  CSV.foreach('./results/' + file_name) do |row|
-    columns = row.length
-    break
+top_dir = ARGV[0] || './runs'
+topologies = Dir.entries(top_dir).select{ |file| !(file == '.' || file == '..')}
+puts topologies
+topologies.each do |top|
+  file_names = Dir.entries(top_dir + '/' + top + '/results').select { |file| file.include? '.csv' }
+  file_names.each do |file_name|
+    columns = 0
+    CSV.foreach(top_dir + '/' + top + '/results/' + file_name) do |row|
+      columns = row.length
+      break
+    end
+    puts columns
+    file_name.slice!('.csv')
+    parts = file_name.split('__')
+    metric = parts[1] 
+    name = './results/' + file_name
+    puts "gnuplot -e \"columns=#{columns - 1} ; metric='#{metric}' ; name= '#{name}'\" plotter.p"
+    output = `gnuplot -e "columns=#{columns - 1} ; metric='#{metric}' ; name= '#{name}'" plotter.p`
+    puts output
   end
-  puts columns
-  file_name.slice!('.csv')
-  parts = file_name.split('__')
-  metric = parts[1] 
-  name = './results/' + file_name
-  puts "gnuplot -e \"columns=#{columns - 1} ; metric='#{metric}' ; name= '#{name}'\" plotter.p"
-  output = `gnuplot -e "columns=#{columns - 1} ; metric='#{metric}' ; name= '#{name}'" plotter.p`
-  puts output
 end
 
 # colnum is the number of columns you want to plot, metric is the y axix label, 
